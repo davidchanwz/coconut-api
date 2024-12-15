04b3a69389333276ff2ee75707810682255bb348
David Chan <davidchan@Davids-Macbook-Air-2.local>
2024-12-15 16:23:49 +0800

    bug fix

---
+++ .render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Use a shared wheel cache to avoid recompiling wheels
+export WHEEL_DIR=/tmp/wheels
+
+# Install packages for building
+pip install --no-cache-dir --upgrade pip
+pip wheel --wheel-dir $WHEEL_DIR -r requirements.txt
+
+# Install from wheel cache
+pip install --no-index --find-links=$WHEEL_DIR -r requirements.txt
